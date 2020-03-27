854d28694bdf061050ca75acb21f2ef1d211e057
alt-source <tahirabdelghafur@gmail.com>
2020-03-27 18:30:31 +0100

    Updates

---
+++ precache-manifest.aeadf8649209d911ce97d0430981251c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01674a90b9e2c8814a969738d1fce4ed",
+    "url": "/Freinds-robots/index.html"
+  },
+  {
+    "revision": "3caa89bf12cce1d1b19e",
+    "url": "/Freinds-robots/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5ed6ee3f05008732794f",
+    "url": "/Freinds-robots/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3caa89bf12cce1d1b19e",
+    "url": "/Freinds-robots/static/js/2.f26bc1ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Freinds-robots/static/js/2.f26bc1ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed6ee3f05008732794f",
+    "url": "/Freinds-robots/static/js/main.3bb53cf3.chunk.js"
+  },
+  {
+    "revision": "9a7d23bf0989e8b7ac34",
+    "url": "/Freinds-robots/static/js/runtime-main.feb16b30.js"
+  }
+]);
